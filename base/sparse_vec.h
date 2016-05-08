ee71265649a07e2a80cc8d3be27c61f12f4968f2
malagbek <malagbek@gmail.com>
2016-05-08 12:24:40 +0200

    Update sparse_vec.h
    Simple unordered_map-based storage for sparse vector
    
    Provides basic methods to set / query elements.

--- base/sparse_vec.h
+++ base/sparse_vec.h
@@ -4,12 +4,14 @@
 #include <unordered_map>
 #include <vector>
 #include <base/macros.h>
+#include <base/sparse_entry.h>
 
 namespace yalp
 {
     /// Container for sparse column / row
     class sparse_vec_um
     {
+        // data storage
         typedef std::unordered_map<int, double> elts_;
     public:
         sparse_vec_um()
@@ -27,16 +29,23 @@ namespace yalp
             return *this;
         }
 
+        size_t size() const
+        {
+            return elts_.size();
+        }
+        
         bool is_empty() const
         {
             return elts_.size() == 0;
         }
         
+        /// Checks whether given id is stored in vector
         bool contains(int i) const
         {
             return elts_.count(i) > 0;
         }
         
+        /// Retrives factor for given id or zero if not found
         double get(int i) const
         {
             auto it = elts_.find(i);
@@ -47,6 +56,29 @@ namespace yalp
             else
             {
                 return 0.0;
+            }
+        }
+        
+        /// Sets factor for given ID.
+        ///
+        /// If v is zero then entry is removed
+        void set(int i, double v)
+        {
+            auto it = elts_.find(i);
+            if (it != elts_.end())
+            {
+                if (v != 0.0)
+                {
+                    it->second = v;
+                }
+                else
+                {
+                    elts_.erase(it);
+                }
+            }
+            else if (v != 0.0)
+            {
+                elts_.insert(std::make_pair(i, v));
             }
         }
         
@@ -74,22 +106,19 @@ namespace yalp
             return it->second;
         }
         
-        /// retrieves sorted lists of indices and factors
-        void get(std::vector<int> &indices, std::vector<double> &factors) const
+        /// retrieves sorted lists of non-zero entries
+        void get_entries(std::vector<sparse_entry_t> &entries) const
         {
-            indices.clear();
-            indices.reserve(elts_.size());
+            entries.clear();
+            entries.reserve(elts_.size());
             for (auto it = elts_.begin(); it != elts_.end(); ++it)
             {
-                indices.push_back(it->first);
+                if (it->second != 0.0)
+                {
+                    entries.push_back(sparse_entry_t(it->first, it->second));
+                }
             }
-            std::sort(indices.begin(), indices.end());
-            factors.clear();
-            factors.reserve(elts_.size());
-            for (auto it = indices.begin(); it != indices.end(); ++it)
-            {
-                factors.push_back(elts_[*it]);
-            }
+            std::sort(entries.begin(), entries.end());
         }
     };
 }
